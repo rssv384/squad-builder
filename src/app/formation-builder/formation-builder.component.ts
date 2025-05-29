c88e420b85311d90c4f23384f00f7fe5f2bf64e1
Raúl Soto <rssotovejar@gmail.com>
2025-05-29 10:04:33 -0700

    Save formation as PNG and JPEG

--- src/app/formation-builder/formation-builder.component.ts
+++ src/app/formation-builder/formation-builder.component.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
+import download from 'downloadjs';
+import { toJpeg as htmlToJpeg, toPng as htmlToPng } from 'html-to-image';
 import { Formation, formations } from './formation.model';
 import { PitchComponent } from './pitch/pitch.component';
 
@@ -10,11 +12,15 @@ import { PitchComponent } from './pitch/pitch.component';
 })
 export class FormationBuilderComponent {
   selectedFormation = signal<Formation>(formations[0]);
+  // formationViewer = pitch + player items
+  private formationViewer =
+    viewChild<ElementRef<HTMLDivElement>>('formationViewer');
 
   get allFormations() {
     return formations;
   }
 
+  // Change formation on select
   onFormationChange(formationId: string) {
     const newSelectedFormation = formations.find(
       (formation) => formation.id === formationId,
@@ -23,5 +29,19 @@ export class FormationBuilderComponent {
     if (newSelectedFormation) {
       this.selectedFormation.set(newSelectedFormation);
     }
+  }
+
+  // Save current formation view as PNG file
+  saveToPNG() {
+    htmlToPng(this.formationViewer()!.nativeElement).then((dataUrl) =>
+      download(dataUrl, this.selectedFormation().id + '.png'),
+    );
+  }
+
+  // Save current formation view as JPEG file
+  saveToJPEG() {
+    htmlToJpeg(this.formationViewer()!.nativeElement, { quality: 0.9 }).then(
+      (dataUrl) => download(dataUrl, this.selectedFormation().id + '.jpeg'),
+    );
   }
 }
