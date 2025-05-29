bf453ecb2f989b169e2e53e0ace507134704ee7b
Raúl Soto <rssotovejar@gmail.com>
2025-05-29 11:18:23 -0700

    Added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
 import { FormationBuilderComponent } from './formation-builder/formation-builder.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FormationBuilderComponent],
+  imports: [HeaderComponent, FormationBuilderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
